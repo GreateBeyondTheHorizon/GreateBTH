c032428702dbef8c280e14715989bda047a45e1b
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-05 00:22:34 +0200

    Changed the way recipes are removed at the startup

--- kubejs/server_scripts/Greg.js
+++ kubejs/server_scripts/Greg.js
@@ -1,25 +1,11 @@
 // priority: 0
 ServerEvents.recipes(event => {
-	//circuits remove
-	event.remove({output: 'gtceu:basic_electronic_circuit'})
-	event.remove({output: 'gtceu:basic_integrated_circuit'})
-	event.remove({output: 'gtceu:good_electronic_circuit'})
-	//lv components remove
-	event.remove({output: 'gtceu:lv_fluid_regulator'})
-	event.remove({output: 'gtceu:lv_emitter'})
-	event.remove({output: 'gtceu:lv_electric_motor'})
-	event.remove({output: 'gtceu:lv_electric_piston'})
-	event.remove({output: 'gtceu:lv_robot_arm'})
-	event.remove({output: 'gtceu:lv_electric_pump'})
-	event.remove({output: 'gtceu:lv_conveyor_module'})
-	event.remove({output: 'gtceu:lv_sensor'})
-	//mv components remove
-	event.remove({output: 'gtceu:mv_electric_motor'})
-	event.remove({output: 'gtceu:mv_electric_piston'})
-	event.remove({output: 'gtceu:mv_robot_arm'})
-	event.remove({output: 'gtceu:mv_conveyor_module'})
-	event.remove({output: 'gtceu:mv_electric_pump'})
-	event.remove({output: 'gtceu:mv_fluid_regulator'})
-	event.remove({output: 'gtceu:mv_sensor'})
-	event.remove({output: 'gtceu:mv_emitter'})
-})
+    const tiers = ['lv', 'mv', /* 'hv', 'ev', 'iv', 'luv', 'zpm', 'uv' */];
+    const components = ['emitter', 'sensor', 'electric_motor', 'electric_piston', 'robot_arm', 'conveyor_module', 'electric_pump', 'fluid_regulator'];
+
+    tiers.forEach(tier => {
+        components.forEach(component => {
+            event.remove({output: `gtceu:${tier}_${component}`});
+        });
+    });
+});
