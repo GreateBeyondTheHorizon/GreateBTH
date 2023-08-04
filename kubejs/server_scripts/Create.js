3fe39d22d766182b20806f6fe6718329c062cbdc
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-05 00:02:40 +0200

    Added sequenced assembly recipes for mv components

--- kubejs/server_scripts/Create.js
+++ kubejs/server_scripts/Create.js
@@ -1,6 +1,7 @@
 // priority: 0
 ServerEvents.recipes(event => {
 	let incLVCirc = 'kubejs:incomplete_lv_circuit'
+	//#region circuits
 	event.recipes.createSequencedAssembly([
 	'gtceu:basic_electronic_circuit',
 ],	'gtceu:resin_printed_circuit_board', [
@@ -8,6 +9,25 @@ ServerEvents.recipes(event => {
 	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:vacuum_tube']),
 	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:red_alloy_single_cable'])
 	]).transitionalItem(incLVCirc).loops(3)
+	event.recipes.createSequencedAssembly([
+	'gtceu:basic_integrated_circuit',
+], 'gtceu:resin_printed_circuit_board',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:resistor']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:diode']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:tin_bolt']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:copper_fine_wire']),
+	event.recipes.createFilling(incLVCirc, [incLVCirc, Fluid.of('gtceu:soldering_alloy', 36)])
+	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:good_electronic_circuit',
+], 'gtceu:phenolic_printed_circuit_board',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:basic_electronic_circuit']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:diode']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:copper_single_wire']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, '#forge:plates/steel'])
+	]).transitionalItem(incLVCirc).loops(3)
+	//#endregion
+	//#region lv components
 	event.recipes.createSequencedAssembly([
 	'gtceu:lv_electric_motor',
 ],	'gtceu:magnetic_iron_rod', [
@@ -76,5 +96,72 @@ ServerEvents.recipes(event => {
 	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:tin_single_cable']),
 	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:brass_rod']),
 	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:brass_rod'])
+]).transitionalItem(incLVCirc).loops(2)
+//#endregion
+
+	//#region mv components
+	event.recipes.createSequencedAssembly([
+		'gtceu:mv_electric_motor',
+], 'gtceu:magnetic_steel_rod',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:cupronickel_double_wire']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:cupronickel_double_wire']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:aluminium_rod']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:copper_single_cable'])
 	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:mv_electric_piston',
+], 'gtceu:mv_electric_motor',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:aluminium_plate']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:aluminium_small_gear']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:aluminium_rod']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:copper_single_cable'])
+	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:mv_robot_arm',
+], 'gtceu:mv_electric_piston',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, '#forge:circuits/mv']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:mv_electric_motor']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:aluminium_rod']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:copper_single_cable'])
+	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:mv_conveyor_module',
+], 'gtceu:copper_single_cable',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:mv_electric_motor']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:rubber_plate']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:rubber_plate']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:rubber_plate']),
+	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:mv_electric_pump',
+], 'gtceu:steel_normal_fluid_pipe',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:bronze_screw']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:bronze_rotor']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:rubber_ring']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:rubber_ring']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:copper_single_cable']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc, 'gtceu:mv_electric_motor']),
+	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+	'gtceu:mv_fluid_regulator',
+],	'gtceu:mv_electric_piston', [
+	event.recipes.createDeploying(incLVCirc, [incLVCirc, '#forge:circuits/mv'])
+	]).transitionalItem(incLVCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:mv_sensor',
+],	'gtceu:electrum_rod',[
+	event.recipes.createDeploying(incLVCirc,[incLVCirc,'#forge:circuits/mv']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc,'gtceu:aluminium_double_plate']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc,'gtceu:aluminium_double_plate']),
+	event.recipes.createDeploying(incLVCirc,[incLVCirc,'gtceu:emerald_flawless_gem']),
+	]).transitionalItem(incLVCirc).loops(1)
+	event.recipes.createSequencedAssembly([
+	'gtceu:mv_emitter',
+],	'gtceu:emerald_flawless_gem', [
+	event.recipes.createDeploying(incLVCirc, [incLVCirc, '#forge:circuits/mv']),
+	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:copper_single_cable']),
+	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:electrum_rod']),
+	event.recipes.createDeploying(incLVCirc, [incLVCirc, 'gtceu:electrum_rod']),
+]).transitionalItem(incLVCirc).loops(2)
+	// #endregion
 })
--- kubejs/server_scripts/Greg.js
+++ kubejs/server_scripts/Greg.js
@@ -1,6 +1,10 @@
 // priority: 0
 ServerEvents.recipes(event => {
+	//circuits remove
 	event.remove({output: 'gtceu:basic_electronic_circuit'})
+	event.remove({output: 'gtceu:basic_integrated_circuit'})
+	event.remove({output: 'gtceu:good_electronic_circuit'})
+	//lv components remove
 	event.remove({output: 'gtceu:lv_fluid_regulator'})
 	event.remove({output: 'gtceu:lv_emitter'})
 	event.remove({output: 'gtceu:lv_electric_motor'})
@@ -9,4 +13,13 @@ ServerEvents.recipes(event => {
 	event.remove({output: 'gtceu:lv_electric_pump'})
 	event.remove({output: 'gtceu:lv_conveyor_module'})
 	event.remove({output: 'gtceu:lv_sensor'})
+	//mv components remove
+	event.remove({output: 'gtceu:mv_electric_motor'})
+	event.remove({output: 'gtceu:mv_electric_piston'})
+	event.remove({output: 'gtceu:mv_robot_arm'})
+	event.remove({output: 'gtceu:mv_conveyor_module'})
+	event.remove({output: 'gtceu:mv_electric_pump'})
+	event.remove({output: 'gtceu:mv_fluid_regulator'})
+	event.remove({output: 'gtceu:mv_sensor'})
+	event.remove({output: 'gtceu:mv_emitter'})
 })
