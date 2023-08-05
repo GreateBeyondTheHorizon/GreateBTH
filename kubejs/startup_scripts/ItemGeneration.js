ebe82558cbc7a35f2253c4a0509e98178b333109
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-05 12:51:15 +0200

    Added custom items for the incomplete products of sequenced assembly

--- kubejs/startup_scripts/ItemGeneration.js
+++ kubejs/startup_scripts/ItemGeneration.js
@@ -1,5 +1,13 @@
 // Priority: 0
 StartupEvents.registry('item', event => {
-	event.create('incomplete_lv_circuit', 'create:sequenced_assembly')
-	event.create('incomplete_lv_motor', 'create:sequenced_assembly')
+	event.create('inc_lv_circuit', 'create:sequenced_assembly')
+
+	event.create('inc_lv_motor', 'create:sequenced_assembly')
+	event.create('inc_lv_piston', 'create:sequenced_assembly')
+	event.create('inc_lv_pump', 'create:sequenced_assembly')
+	event.create('inc_lv_fluid_regulator', 'create:sequenced_assembly')
+	event.create('inc_lv_sensor', 'create:sequenced_assembly')
+	event.create('inc_lv_emitter', 'create:sequenced_assembly')
+	event.create('inc_lv_conveyor', 'create:sequenced_assembly')
+	event.create('inc_lv_robot_arm', 'create:sequenced_assembly')
 })
