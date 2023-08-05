b1a5f2b60ddd6ec4e41fd4987be4e089bd560524
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-05 15:07:58 +0200

    Added missing recipes for diffrent rubber types

--- kubejs/server_scripts/LvSequenceAssembly.js
+++ kubejs/server_scripts/LvSequenceAssembly.js
@@ -1,5 +1,6 @@
 // priority: 0
 ServerEvents.recipes(event=>{
+	let incLvCirc = 'kubejs:inc_lv_circuit'
     let incLvMotor = 'kubejs:inc_lv_motor'
 	let incLvPiston = 'kubejs:inc_lv_piston'
 	let lncLvRobotArm = 'kubejs:inc_lv_robot_arm'
@@ -8,6 +9,60 @@ ServerEvents.recipes(event=>{
 	let invLvFluidRegulator = 'kubejs:inc_lv_fluid_regulator'
 	let invLvSensor = 'kubejs:inc_lv_sensor'
 	let incLvEmitter = 'kubejs:inc_lv_emitter'
+
+	let sub2 = 'kubejs:sub_lv_circuit_2'
+	let sub3 = 'kubejs:sub_lv_circuit_3'
+
+
+	event.recipes.createMixing([sub2],
+		[
+			'gtceu:resin_printed_circuit_board',
+			'gtceu:ilc_chip', 
+			Fluid.of('gtceu:soldering_alloy', 72)
+		])
+	event.recipes.createMixing([sub3],
+		[
+			'gtceu:plastic_printed_circuit_board',
+			'gtceu:cpu_chip', 
+			Fluid.of('gtceu:soldering_alloy', 72)
+		])
+	event.recipes.createMixing([sub2],
+		[
+			'gtceu:resin_printed_circuit_board',
+			'gtceu:ilc_chip', 
+			Fluid.of('gtceu:tin', 144)
+		])
+	event.recipes.createMixing([sub3],
+		[
+			'gtceu:plastic_printed_circuit_board',
+			'gtceu:cpu_chip', 
+			Fluid.of('gtceu:tin', 144)
+		])
+	// #region circuits
+	event.recipes.createSequencedAssembly([
+		'gtceu:basic_electronic_circuit',
+],	'gtceu:resin_printed_circuit_board', [
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:red_alloy_single_wire']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:resistor']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:vacuum_tube']),
+	]).transitionalItem(incLvCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:basic_integrated_circuit',
+],	sub2, [
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:resistor']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:diode']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:copper_fine_wire']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:tin_bolt']),
+	]).transitionalItem(incLvCirc).loops(2)
+	event.recipes.createSequencedAssembly([
+		'gtceu:microchip_processor',
+],	sub3, [
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:resistor']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:capacitor']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:transistor']),
+	event.recipes.createDeploying(incLvCirc, [incLvCirc, 'gtceu:copper_fine_wire']),
+	]).transitionalItem(incLvCirc).loops(2)
+	// #endregion
     event.recipes.createSequencedAssembly([
 	'gtceu:lv_electric_motor',
 ],	'gtceu:magnetic_iron_rod', [
@@ -47,6 +102,22 @@ ServerEvents.recipes(event=>{
 	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:lv_electric_motor'])
 	]).transitionalItem(incLvConveyor).loops(2)
 	event.recipes.createSequencedAssembly([
+	'gtceu:lv_conveyor_module',
+],	'gtceu:tin_single_cable', [
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:silicone_rubber_plate']),
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:silicone_rubber_plate']),
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:silicone_rubber_plate']),
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:lv_electric_motor'])
+	]).transitionalItem(incLvConveyor).loops(2)
+	event.recipes.createSequencedAssembly([
+	'gtceu:lv_conveyor_module',
+],	'gtceu:tin_single_cable', [
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:styrene_butadiene_rubber_plate']),
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:styrene_butadiene_rubber_plate']),
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:styrene_butadiene_rubber_plate']),
+	event.recipes.createDeploying(incLvConveyor, [incLvConveyor, 'gtceu:lv_electric_motor'])
+	]).transitionalItem(incLvConveyor).loops(2)
+	event.recipes.createSequencedAssembly([
 	'gtceu:lv_electric_pump',
 ],	'gtceu:bronze_normal_fluid_pipe', [
 	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:lv_electric_motor']),
@@ -57,13 +128,33 @@ ServerEvents.recipes(event=>{
 	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_single_cable'])
 	]).transitionalItem(incLvPump).loops(2)
 	event.recipes.createSequencedAssembly([
+	'gtceu:lv_electric_pump',
+],	'gtceu:bronze_normal_fluid_pipe', [
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:lv_electric_motor']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_screw']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_rotor']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:silicone_rubber_ring']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:silicone_rubber_ring']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_single_cable'])
+	]).transitionalItem(incLvPump).loops(2)
+	event.recipes.createSequencedAssembly([
+	'gtceu:lv_electric_pump',
+],	'gtceu:bronze_normal_fluid_pipe', [
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:lv_electric_motor']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_screw']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_rotor']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:styrene_butadiene_rubber_ring']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:styrene_butadiene_rubber_ring']),
+	event.recipes.createDeploying(incLvPump, [incLvPump, 'gtceu:tin_single_cable'])
+	]).transitionalItem(incLvPump).loops(2)
+	event.recipes.createSequencedAssembly([
 	'gtceu:lv_fluid_regulator',
 ],	'gtceu:lv_electric_piston', [
 	event.recipes.createDeploying(invLvFluidRegulator, [invLvFluidRegulator, '#forge:circuits/lv'])
 	]).transitionalItem(invLvFluidRegulator).loops(2)
 	event.recipes.createSequencedAssembly([
 	'gtceu:lv_sensor',
-],	'gtceu:basic_electronic_circuit', [
+],	'#forge:circuits/lv', [
 	event.recipes.createDeploying(invLvSensor, [invLvSensor, 'gtceu:steel_double_plate']),
 	event.recipes.createDeploying(invLvSensor, [invLvSensor, 'gtceu:steel_double_plate']),
 	event.recipes.createDeploying(invLvSensor, [invLvSensor, 'gtceu:brass_rod']),
