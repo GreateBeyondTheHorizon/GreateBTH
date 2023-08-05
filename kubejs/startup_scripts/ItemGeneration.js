3ee12b5935ef8421f76fd2798a891bf361382c50
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-05 15:06:13 +0200

    Added incomplete items for MV sequenced assembly and sub components for tier 2&3 LV circuits

--- kubejs/startup_scripts/ItemGeneration.js
+++ kubejs/startup_scripts/ItemGeneration.js
@@ -1,7 +1,9 @@
 // Priority: 0
 StartupEvents.registry('item', event => {
 	event.create('inc_lv_circuit', 'create:sequenced_assembly')
-
+	event.create('sub_lv_circuit_2')
+	event.create('sub_lv_circuit_3')
+	// LV components
 	event.create('inc_lv_motor', 'create:sequenced_assembly')
 	event.create('inc_lv_piston', 'create:sequenced_assembly')
 	event.create('inc_lv_pump', 'create:sequenced_assembly')
@@ -10,4 +12,13 @@ StartupEvents.registry('item', event => {
 	event.create('inc_lv_emitter', 'create:sequenced_assembly')
 	event.create('inc_lv_conveyor', 'create:sequenced_assembly')
 	event.create('inc_lv_robot_arm', 'create:sequenced_assembly')
+	// MV components
+	event.create('inc_mv_motor', 'create:sequenced_assembly')
+	event.create('inc_mv_piston', 'create:sequenced_assembly')
+	event.create('inc_mv_pump', 'create:sequenced_assembly')
+	event.create('inc_mv_fluid_regulator', 'create:sequenced_assembly')
+	event.create('inc_mv_sensor', 'create:sequenced_assembly')
+	event.create('inc_mv_emitter', 'create:sequenced_assembly')
+	event.create('inc_mv_conveyor', 'create:sequenced_assembly')
+	event.create('inc_mv_robot_arm', 'create:sequenced_assembly')
 })
