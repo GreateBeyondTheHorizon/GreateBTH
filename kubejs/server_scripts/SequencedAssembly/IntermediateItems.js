fdcbbac0dbf9650f58d8b6bded7697570a7b4eb0
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-07 21:50:53 +0200

    Added recipes for HV circuits and necessary intermediate items and raw circuits boards

--- kubejs/server_scripts/SequencedAssembly/IntermediateItems.js
+++ kubejs/server_scripts/SequencedAssembly/IntermediateItems.js
@@ -3,6 +3,10 @@ ServerEvents.recipes(event => {
     let rawIntegratedCirc = 'kubejs:raw_integrated_circuit_board'
 	let rawPlasticCircBoard = 'kubejs:raw_plastic_circuit_board'
     let rawPhenolicCircBoard = 'kubejs:raw_phenolic_circuit_board'
+	let interAdvCirc = 'kubejs:inter_advanced_integrated_circuit'
+	let interMicroProcCirc = 'kubejs:inter_micro_processor_assembly'
+	let rawEpoxyCircBoard = 'kubejs:raw_epoxy_circuit_board'
+	let interNanoProc = 'kubejs:inter_nano_processor'
 
     event.recipes.createMixing([rawIntegratedCirc],
 		[
@@ -46,5 +50,67 @@ ServerEvents.recipes(event => {
 	event.recipes.createFilling(rawPhenolicCircBoard, [rawPhenolicCircBoard, Fluid.of('gtceu:tin', 72)])
 	]).transitionalItem(rawPhenolicCircBoard).loops(2)
 
+	event.recipes.createMixing([interAdvCirc],
+		[
+			'2x gtceu:good_integrated_circuit',
+			'2x gtceu:ram_chip',
+			'2x gtceu:ilc_chip',
+			Fluid.of('gtceu:tin', 144)
+		]).heated()
+	event.recipes.createMixing([interAdvCirc],
+		[
+			'2x gtceu:good_integrated_circuit',
+			'2x gtceu:ram_chip',
+			'2x gtceu:ilc_chip',
+			Fluid.of('gtceu:soldering_alloy', 72)
+		]).heated()
+
+	event.recipes.createSequencedAssembly([
+		interMicroProcCirc
+],	rawPlasticCircBoard, [
+	event.recipes.createDeploying(rawPlasticCircBoard, [rawPlasticCircBoard, 'gtceu:micro_processor']),
+	event.recipes.createDeploying(rawPlasticCircBoard, [rawPlasticCircBoard, 'gtceu:ram_chip']),
+	event.recipes.createDeploying(rawPlasticCircBoard, [rawPlasticCircBoard, 'gtceu:ram_chip']),
+	event.recipes.createFilling(rawPlasticCircBoard, [rawPlasticCircBoard, Fluid.of('gtceu:tin', 144)])
+	]).transitionalItem(rawPlasticCircBoard).loops(2)
+
+	event.recipes.createSequencedAssembly([
+		interMicroProcCirc
+],	rawPlasticCircBoard, [
+	event.recipes.createDeploying(rawPlasticCircBoard, [rawPlasticCircBoard, 'gtceu:micro_processor']),
+	event.recipes.createDeploying(rawPlasticCircBoard, [rawPlasticCircBoard, 'gtceu:ram_chip']),
+	event.recipes.createDeploying(rawPlasticCircBoard, [rawPlasticCircBoard, 'gtceu:ram_chip']),
+	event.recipes.createFilling(rawPlasticCircBoard, [rawPlasticCircBoard, Fluid.of('gtceu:soldering_alloy', 72)])
+	]).transitionalItem(rawPlasticCircBoard).loops(2)
 	
+	event.recipes.createMixing([rawEpoxyCircBoard],
+		[
+			'gtceu:epoxy_printed_circuit_board',
+			'gtceu:nano_cpu_chip',
+			Fluid.of('gtceu:soldering_alloy', 72)
+		]).heated()
+	event.recipes.createMixing([rawEpoxyCircBoard],
+		[
+			'gtceu:epoxy_printed_circuit_board',
+			'gtceu:nano_cpu_chip',
+			Fluid.of('gtceu:tin', 144)
+		]).heated()
+		
+	event.recipes.createSequencedAssembly([
+		interNanoProc
+],	rawEpoxyCircBoard, [
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:smd_resistor']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:smd_capacitor']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:smd_transistor']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:electrum_fine_wire']),
+	]).transitionalItem(rawEpoxyCircBoard).loops(6)
+	event.recipes.createSequencedAssembly([
+		interNanoProc
+],	rawEpoxyCircBoard, [
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:advanced_smd_resistor']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:advanced_smd_capacitor']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:advanced_smd_transistor']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:electrum_fine_wire']),
+	event.recipes.createDeploying(rawEpoxyCircBoard, [rawEpoxyCircBoard, 'gtceu:electrum_fine_wire']),
+	]).transitionalItem(rawEpoxyCircBoard).loops(2)
 })
