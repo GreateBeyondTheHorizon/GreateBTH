2e4fd08d54c56eb0d7a20c9cc923203303b92a28
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-07 21:50:53 +0200

    Added recipes for HV circuits and necessary intermediate items and raw circuits boards

--- kubejs/startup_scripts/ItemGeneration.js
+++ kubejs/startup_scripts/ItemGeneration.js
@@ -9,11 +9,15 @@ StartupEvents.registry('item', event => {
 	event.create('inc_good_integrated_circuit', 'create:sequenced_assembly')
 	event.create('inc_micro_processor', 'create:sequenced_assembly')
 
-	event.create('inc_hv_circuit', 'create:sequenced_assembly')
+	event.create('inter_advanced_integrated_circuit')
+	event.create('inter_micro_processor_assembly')
+	event.create('inter_nano_processor')
+
 	
 	event.create('raw_integrated_circuit_board').displayName("Raw Integrated Circuit Board")
 	event.create('raw_phenolic_circuit_board', 'create:sequenced_assembly').displayName("Raw Phenolic Printed Circuit Board")
 	event.create('raw_plastic_circuit_board').displayName("Raw Plastic Printed Circuit Board")
+	event.create('raw_epoxy_circuit_board').displayName("Raw Epoxy Printed Circuit Board")
 	// LV components
 	event.create('inc_lv_motor', 'create:sequenced_assembly')
 	event.create('inc_lv_piston', 'create:sequenced_assembly')
@@ -36,6 +40,4 @@ StartupEvents.registry('item', event => {
 	event.create('unf_turtle_helmet').displayName('Turtle Helmet Shell')
 	event.create('netherite_plate').displayName('Netherite Plate')
 
-	event.create('bed_base').displayName('Bed Base')
-	event.create('mattress').displayName('Mattress')
 })
