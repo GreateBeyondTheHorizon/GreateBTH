88f130eb9ea5287bec5b647995ab37a4b9636b93
domi-grb <dominik.grabher@outlook.com>
2023-08-11 13:32:14 +0200

    TomsSimpleStorage

---
+++ kubejs/client_scripts/rei_hide.js
@@ -0,0 +1,21 @@
+REIEvents.hide('item', event => {
+
+
+
+
+
+
+    //Create
+    const sheet = ['create:iron_sheet','create:brass_sheet','create:copper_sheet','create:golden_sheet']
+    sheet.forEach((element) => event.hide(element));
+
+    //Wands
+    event.hide('constructionwand:core_angel')
+    event.hide('constructionwand:core_destruction')
+
+    //TomSimpleStorage
+    const storage = ['toms_storage:ts.inventory_proxy','toms_storage:ts.open_crate', 'toms_storage:ts.inventory_hopper_basic', 'toms_storage:ts.level_emitter', 'toms_storage:ts.painted_trim']
+    storage.forEach((element) => event.hide(element));
+
+
+  })
