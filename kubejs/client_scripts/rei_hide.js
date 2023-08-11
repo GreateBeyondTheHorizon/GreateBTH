bc168954470cf93fff5448241a97fc0760ed2f54
domi-grb <dominik.grabher@outlook.com>
2023-08-11 14:05:18 +0200

    fix

--- kubejs/client_scripts/rei_hide.js
+++ kubejs/client_scripts/rei_hide.js
@@ -14,7 +14,7 @@ REIEvents.hide('item', event => {
     event.hide('constructionwand:core_destruction')
 
     //TomSimpleStorage
-    const storage = ['toms_storage:ts.inventory_proxy','toms_storage:ts.open_crate', 'toms_storage:ts.inventory_hopper_basic', 'toms_storage:ts.level_emitter', 'toms_storage:ts.painted_trim']
+    const storage = ['toms_storage:ts.inventory_proxy','toms_storage:ts.open_crate', 'toms_storage:ts.inventory_hopper_basic', 'toms_storage:ts.level_emitter', 'toms_storage:ts.painted_trim', 'toms_storage:ts.storage_terminal', 'toms_storage:ts.adv_wireless_terminal']
     storage.forEach((element) => event.hide(element));
 
 
