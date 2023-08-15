a3b357fd0b06bcb9762698770fadcd252a63bdef
domi-grb <dominik.grabher@outlook.com>
2023-08-15 11:24:38 +0200

    Backpacks

--- kubejs/client_scripts/CreateHide.js
+++
@@ -1,8 +0,0 @@
-REIEvents.hide('item', event => {
-
-    const sheet = ['create:iron_sheet','create:brass_sheet','create:copper_sheet','create:golden_sheet']
-    
-
-    sheet.forEach((element) => event.hide(element));
-    
-  })
