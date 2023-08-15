9e3152d1663971ebb6fa6105876060331c3dd5d8
domi-grb <dominik.grabher@outlook.com>
2023-08-15 11:24:38 +0200

    Backpacks

--- kubejs/server_scripts/Mods/TomsSimpleStorage.js
+++ kubejs/server_scripts/Mods/TomsSimpleStorage.js
@@ -1,7 +1,7 @@
 ServerEvents.recipes(event => {
 
-    //remove all    'toms_storage:ts.polymorphic_item_filter', 
-    const storage = ['toms_storage:ts.inventory_connector', 'toms_storage:ts.storage_terminal', 'toms_storage:ts.crafting_terminal', 'toms_storage:ts.item_filter',
+    //remove all    
+    const storage = ['toms_storage:ts.inventory_connector', 'toms_storage:ts.storage_terminal', 'toms_storage:ts.crafting_terminal', 'toms_storage:ts.item_filter', 'toms_storage:ts.polymorphic_item_filter', 
                         'toms_storage:ts.trim', 'toms_storage:ts.open_crate',  'toms_storage:ts.inventory_cable', 'toms_storage:ts.tag_item_filter','toms_storage:ts.painted_trim',
                         'toms_storage:ts.inventory_cable_connector', 'toms_storage:ts.inventory_proxy', 'toms_storage:ts.inventory_hopper_basic', 'toms_storage:ts.level_emitter', 
                         'toms_storage:ts.wireless_terminal', 'toms_storage:ts.adv_wireless_terminal',  'toms_storage:ts.paint_kit','toms_storage:ts.inventory_cable_connector_filtered']
