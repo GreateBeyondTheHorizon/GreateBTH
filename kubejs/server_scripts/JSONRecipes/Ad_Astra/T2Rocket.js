c0958955cc1955c5a283c6cd985ecf92076a806e
GamesGrandpa <trashraynormain@gmail.com>
2023-08-16 17:50:42 -0400

    Gregified Rocket Recipes
    in addition armor.js was removed as it's now redundant

---
+++ kubejs/server_scripts/JSONRecipes/Ad_Astra/T2Rocket.js
@@ -0,0 +1,81 @@
+ServerEvents.recipes (event => {
+	event.custom({
+		
+		
+	"type": "ad_astra:nasa_workbench",
+	"ingredients": [
+		{
+			"ingredient": {
+				"item": 'kubejs:tungsten_steel_cone'
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:tungsten_carbide_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:tungsten_carbide_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:tungsten_carbide_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:tungsten_carbide_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:tungsten_carbide_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:tungsten_carbide_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:tungsten_steel_fin"
+			}
+		},
+		{
+			"ingredient": {
+				"item": 'gtceu:ev_super_tank'
+			}
+		},
+		{
+			"ingredient": {
+				"item": 'gtceu:ev_super_tank'
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:tungsten_steel_fin"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:tungsten_steel_fin"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:tungsten_steel_engine"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:tungsten_steel_fin"
+			}
+		}
+	],
+	"output": "ad_astra:tier_2_rocket"
+	})
+	
+})
---
+++ kubejs/server_scripts/JSONRecipes/Ad_Astra/T3Rocket.js
@@ -0,0 +1,81 @@
+ServerEvents.recipes (event => {
+	event.custom({
+		
+		
+	"type": "ad_astra:nasa_workbench",
+	"ingredients": [
+		{
+			"ingredient": {
+				"item": 'kubejs:hsse_cone'
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:hsse_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:hsse_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:hsse_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:hsse_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:hsse_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "gtceu:hsse_block"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:hsse_fin"
+			}
+		},
+		{
+			"ingredient": {
+				"item": 'gtceu:iv_quantum_tank'
+			}
+		},
+		{
+			"ingredient": {
+				"item": 'gtceu:iv_quantum_tank'
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:hsse_fin"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:hsse_fin"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:hsse_engine"
+			}
+		},
+		{
+			"ingredient": {
+				"item": "kubejs:hsse_fin"
+			}
+		}
+	],
+	"output": "ad_astra:tier_3_rocket"
+	})
+	
+})
