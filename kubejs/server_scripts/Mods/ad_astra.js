fb7500c2b5cefbd8c651272dd57393649b8a5f2d
GamesGrandpa <trashraynormain@gmail.com>
2023-08-16 20:19:12 -0400

    More Ad Astra recipes

--- kubejs/server_scripts/Mods/ad_astra.js
+++ kubejs/server_scripts/Mods/ad_astra.js
@@ -1,12 +1,29 @@
 ServerEvents.recipes( event => {
 	
 	event.remove({ output:'ad_astra:rocket_fin' })
-	event.remove({ output:'ad_astra:steel_engine' })
+	event.remove({ output:/ad_astra:(.*)engine/ })
 	event.remove({ output:'ad_astra:rocket_nose_cone' })
 	
 	//stainless steel
 	
+	
 	event.shaped(
+	Item.of('kubejs:stainless_steel_engine', 1),
+		[
+			'AAA',
+			' B ',
+			'DCD'
+		],
+		{
+			A:'gtceu:stainless_steel_double_plate',
+			B:'gtceu:stainless_steel_frame',
+			C:'gtceu:stainless_steel_rotor',
+			D:'#forge:circuits/hv'
+		}
+	)
+	
+	
+	event.shaped(	
 		Item.of('kubejs:stainless_steel_fin', 1),
 		[
 			' A ',
@@ -31,6 +48,21 @@ ServerEvents.recipes( event => {
 	)
 	
 	//tungsten steel/carbide
+	
+	event.shaped(
+	Item.of('kubejs:tungsten_steel_engine', 1),
+		[
+			'AAA',
+			' B ',
+			'DCD'
+		],
+		{
+			A:'gtceu:tungsten_steel_double_plate',
+			B:'gtceu:tungsten_steel_frame',
+			C:'gtceu:tungsten_steel_rotor',
+			D:'#forge:circuits/ev'
+		}
+	)
 	
 	event.shaped(
 		Item.of('kubejs:tungsten_steel_fin', 1),
@@ -59,6 +91,21 @@ ServerEvents.recipes( event => {
 	// HSS-E
 	
 	event.shaped(
+	Item.of('kubejs:hsse_engine', 1),
+		[
+			'AAA',
+			' B ',
+			'DCD'
+		],
+		{
+			A:'gtceu:hsse_double_plate',
+			B:'gtceu:hsse_frame',
+			C:'kubejs:hsse_rotor',
+			D:'#forge:circuits/iv'
+		}
+	)
+	
+	event.shaped(
 		Item.of('kubejs:hsse_fin', 1),
 		[
 			' A ',
@@ -83,6 +130,21 @@ ServerEvents.recipes( event => {
 	)
 	
 	// Trinium
+	
+	event.shaped(
+	Item.of('kubejs:trinium_engine', 1),
+		[
+			'AAA',
+			' B ',
+			'DCD'
+		],
+		{
+			A:'gtceu:trinium_double_plate',
+			B:'kubejs:trinium_frame',
+			C:'kubejs:trinium_rotor',
+			D:'#forge:circuits/luv'
+		}
+	)
 	
 	event.shaped(
 		Item.of('kubejs:trinium_fin'),
