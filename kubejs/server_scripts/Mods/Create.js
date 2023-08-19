1d2924c12dfca98cb13278bf00ef8af361260540
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-19 22:19:32 +0200

    Recipes for water wheels and windmill

--- kubejs/server_scripts/Mods/Create.js
+++ kubejs/server_scripts/Mods/Create.js
@@ -1,28 +1,76 @@
 ServerEvents.recipes(event => {
 
-event.remove({ output: 'create:iron_sheet'})
-event.remove({ output: 'create:brass_sheet'})
-event.remove({ output: 'create:copper_sheet'})
-event.remove({ output: 'create:golden_sheet'})
-	
-event.recipes.createMixing('minecraft:netherite_ingot', [
-  'minecraft:netherite_scrap',
-  'minecraft:netherite_scrap',
-  'minecraft:netherite_scrap',
-  'minecraft:netherite_scrap',
-  Fluid.of('gtceu:hssg', 576)
-]).superheated()
+  event.remove({ output: 'create:iron_sheet'})
+  event.remove({ output: 'create:brass_sheet'})
+  event.remove({ output: 'create:copper_sheet'})
+  event.remove({ output: 'create:golden_sheet'})
+    
+  event.recipes.createMixing('minecraft:netherite_ingot', [
+    'minecraft:netherite_scrap',
+    'minecraft:netherite_scrap',
+    'minecraft:netherite_scrap',
+    'minecraft:netherite_scrap',
+    Fluid.of('gtceu:hssg', 576)
+  ]).superheated()
 
-event.recipes.createMixing('minecraft:turtle_helmet', [
-  'kubejs:unf_turtle_helmet',
-  Fluid.of('create:potion', 576, {Bottle:'REGULAR', Potion:'minecraft:water_breathing'})
-]).heated()
-
-
+  event.recipes.createMixing('minecraft:turtle_helmet', [
+    'kubejs:unf_turtle_helmet',
+    Fluid.of('create:potion', 576, {Bottle:'REGULAR', Potion:'minecraft:water_breathing'})
+  ]).heated()
 
 
-
-
-
-
+  // SU gen
+  // water wheel small
+  event.shaped(	
+      Item.of('create:water_wheel', 1),
+      [
+        'DCE',
+        'CAC',
+        'BCB'
+      ],
+      {
+        A:'create:andesite_casing',
+        B:'gtceu:wood_screw',
+        C:'gtceu:wood_plate',
+        D:'#forge:tools/screwdrivers',
+        E:'#forge:tools/files',
+      }
+  )
+  //water wheel large
+    event.recipes.createMechanicalCrafting(
+		Item.of('create:large_water_wheel', 1),
+		[ 
+      ' CDC ',
+      'CEBEC',
+      'DBABD',
+      'CEBEC',
+      ' CDC ',
+		],
+		{
+      A:'create:water_wheel',
+      B:'gtceu:steel_frame',
+      C:'gtceu:steel_plate',
+      D:'gtceu:wood_frame',
+      E:'create:large_cogwheel',
+    }
+	)
+  // windmill bearing
+    event.recipes.createMechanicalCrafting(
+		Item.of('create:windmill_bearing', 1),
+		[ 
+      ' FEF ',
+      'FDCDF',
+      'EBABE',
+      'FDCDF',
+      ' FEF ',
+		],
+		{
+      A:'gtceu:mv_machine_casing',
+      B:'create:large_cogwheel',
+      C:'create:cogwheel',
+      D:'gtceu:aluminium_plate',
+      E:'gtceu:aluminium_rod',
+      F:'gtceu:aluminium_small_gear',
+    }
+	)
 })
