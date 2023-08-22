3ea2b787bbc66ce55725dc517d80e907bcd15a72
domi-grb <dominik.grabher@outlook.com>
2023-08-22 19:30:57 +0200

    extra outputs for crushing wheel

--- kubejs/server_scripts/Ore Processing/RawCrushing.js
+++ kubejs/server_scripts/Ore Processing/RawCrushing.js
@@ -5,24 +5,24 @@ ServerEvents.recipes(event => {
     //Minecraft:
     const vanillaMaterial = ['gold', 'iron', 'copper']
     
-    const vanExtraMaterial = []
+    const vanExtraMaterial = ['copper', 'nickel', 'cobalt']
 
     //Greg:
-    const gregMaterial = ['aluminium','beryllium','cobalt','lead','lithium','molybdenum','neodymium','nickel','palladium','platinum','plutonium','silver','sulfur','thorium','tin','naquadah','asbesutos','hematite','geothite','calcite','cassiterite','cassiterite_sand','chalcopyrite','chromite','cobaltite','cooperite','galena','garnierite','ilmenite','bauxite','magnesite','magnetite','molybdenite','powellite','pyrite','pyrolusite','saltpeter','scheelite','tantalite','sphalerite','stibnite','tetrahedrite','tungstate','uraninite','wulfenite','yellow_limonite','graphite','bornite','chalcocite','bastnasite','pentlandite','spodumene','lepidolite','glauconite_sand','mica','barite','alunite','talc','soapstone','kyanite','pyrochlore','oilsands','tricalcium_phosphate','vanadium_magnetite','pollucite','bentonite','fullers_earth','pitchblende','trona','gypsum','zeolite','redstone','electrotine','diatomite','granitic_mineral_sand','garnet_sand','basaltic_mineral_sand']
+    const gregMaterial = ['aluminium','beryllium','cobalt','lead','lithium','molybdenum','neodymium','nickel','palladium','platinum','plutonium','silver','goethite','sulfur','thorium','tin','naquadah','asbesutos','hematite','geothite','calcite','cassiterite','cassiterite_sand','chalcopyrite','chromite','cobaltite','cooperite','galena','garnierite','ilmenite','bauxite','magnesite','magnetite','molybdenite','powellite','pyrite','pyrolusite','saltpeter','scheelite','tantalite','sphalerite','stibnite','tetrahedrite','tungstate','uraninite','wulfenite','yellow_limonite','graphite','bornite','chalcocite','bastnasite','pentlandite','spodumene','lepidolite','glauconite_sand','mica','barite','alunite','talc','soapstone','kyanite','pyrochlore','oilsands','tricalcium_phosphate','vanadium_magnetite','pollucite','bentonite','fullers_earth','pitchblende','trona','gypsum','zeolite','redstone','electrotine','diatomite','granitic_mineral_sand','garnet_sand','basaltic_mineral_sand']
 
-    const extraMaterial = []
+    const extraMaterial = ['bauxite','emerald','cobalt_oxide','silver','lithium','molybdenum','rare_earth','cobalt','palladium','nickel','uraninite','lead','sulfur','uraninite','iron','sulfur','red_garnet','diatomite','magnetite','topaz','malachite','calcium','tin','tin','pyrite','iron','redstone','coal','sulfur','nickel','graphite','beryllium','sulfur','iron','aluminium','yellow_garnet','iron','grossular','sodalite','magnesium','iron','molybdenum','iron','sulfur','manganese','red_garnet','salt','chromium','rock_salt','saltpeter','aluminium','manganese','lazurite','manganese','red_garnet','yellow_garnet','antimony_trrioxide','antimony','blue_topaz','manganese','uraninite','iron','nickel','quartzite','nether_quartz','certus_quartz','carbon','pyrite','sulfur','aluminium','cinnabar','thorium','magnetite','redstone','tin','copper','copper','copper','copper']
 
     //Minecraft
     let i = 0;
-    for(i; i < vanillamaterial.length; i++){
-        event.recipes.create.crushing([`gtceu:${vanillaMaterial[i]}_crushed_ore`,Item.of(`gtceu:${vanillaMaterial[i]}_crushed_ore`).withChance(0.5), Item.of(`gtceu:${vanillaMaterial[i]}_crushed_ore`).withChance(0.25),Item.of(`gtceu:${vanillaMaterial[j]}_impure_dust`).withChance(0.2),Item.of(`gtceu:${vanExtraMaterial[j]}_crushed_ore`).withChance(0.2)], `minecraft:raw_${vanillaMaterial[i]}`)
+    for(i; i < vanillaMaterial.length; i++){
+        event.recipes.create.crushing([`gtceu:${vanillaMaterial[i]}_crushed_ore`,Item.of(`gtceu:${vanillaMaterial[i]}_crushed_ore`).withChance(0.5), Item.of(`gtceu:${vanillaMaterial[i]}_crushed_ore`).withChance(0.25),Item.of(`gtceu:${vanillaMaterial[i]}_impure_dust`).withChance(0.2),Item.of(`gtceu:${vanExtraMaterial[i]}_dust`).withChance(0.2)], `minecraft:raw_${vanillaMaterial[i]}`)
         event.recipes.create.milling([`gtceu:${vanillaMaterial[i]}_crushed_ore`, Item.of(`gtceu:${vanillaMaterial[i]}_crushed_ore`).withChance(0.5)], `minecraft:raw_${vanillaMaterial[i]}`)
     }
 
     let j = 0;
 
     for(j; j < gregMaterial.length; j++){
-        event.recipes.create.crushing([`gtceu:${gregMaterial[j]}_crushed_ore`,Item.of(`gtceu:${gregMaterial[j]}_crushed_ore`).withChance(0.5), Item.of(`gtceu:${gregMaterial[j]}_crushed_ore`).withChance(0.25),Item.of(`gtceu:${gregMaterial[j]}_impure_dust`).withChance(0.2),Item.of(`gtceu:${extraMaterial[j]}_crushed_ore`).withChance(0.2)],  `gtceu:raw_${gregMaterial[j]}`)
+        event.recipes.create.crushing([`gtceu:${gregMaterial[j]}_crushed_ore`,Item.of(`gtceu:${gregMaterial[j]}_crushed_ore`).withChance(0.5), Item.of(`gtceu:${gregMaterial[j]}_crushed_ore`).withChance(0.25),Item.of(`gtceu:${gregMaterial[j]}_impure_dust`).withChance(0.2),Item.of(`gtceu:${extraMaterial[j]}_dust`).withChance(0.2)],  `gtceu:raw_${gregMaterial[j]}`)
         event.recipes.create.milling([`gtceu:${gregMaterial[j]}_crushed_ore`, Item.of(`gtceu:${gregMaterial[j]}_crushed_ore`).withChance(0.10)], 'gtceu:raw_'+gregMaterial[j])
     }
 
