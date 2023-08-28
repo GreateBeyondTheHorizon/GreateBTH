6177867863ce78967766d49a89a6f5373321646e
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-08-28 12:26:48 +0200

    Tool-less recipes for greate cogs

--- kubejs/server_scripts/Mods/Greate.js
+++ kubejs/server_scripts/Mods/Greate.js
@@ -1,4 +1,40 @@
 // priority: 0
 ServerEvents.recipes(event => {
-    
-})
+
+    let tiers = [
+        'andesite',
+        'steel',
+        'aluminium',
+        'stainless_steel',
+        'titanium',
+        //'tungsten_steel',
+        'palladium',
+        'naquadah',
+        'darmstadtium',
+        'neutronium'
+    ];
+
+    let plates = [
+        'wood',
+        'stone',
+        'steel',
+        'aluminium',
+        'stainless_steel',
+        'titanium',
+        //'tungsten_steel',
+        'palladium',
+        'naquadah',
+        'darmstadtium'
+    ];
+
+    tiers.forEach((tier, index) => {
+        event.recipes.createDeploying(`greate:${tier}_cogwheel`, [`greate:${tier}_shaft`, `gtceu:${plates[index]}_plate`]);
+        event.recipes.createDeploying(`greate:large_${tier}_cogwheel`, [`greate:${tier}_cogwheel`, `gtceu:${plates[index]}_plate`]);
+        event.recipes.createSequencedAssembly([
+            `greate:large_${tier}_cogwheel`
+        ],  `greate:${tier}_shaft`, [
+            event.recipes.createDeploying(`greate:${tier}_cogwheel`, [`greate:${tier}_cogwheel`, `gtceu:${plates[index]}_plate`])
+        ]).transitionalItem(`greate:${tier}_cogwheel`).loops(2)
+    });
+
+});
