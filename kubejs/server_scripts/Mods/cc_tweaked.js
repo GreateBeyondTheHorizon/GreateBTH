c70d4af029d5e89de57ba2c70838e3698c15162c
lonevox <quinn.epicness@gmail.com>
2023-09-08 16:58:30 +1200

    Gregify disk drive recipe

--- kubejs/server_scripts/Mods/cc_tweaked.js
+++ kubejs/server_scripts/Mods/cc_tweaked.js
@@ -7,6 +7,14 @@ ServerEvents.recipes( event => {
         .duration(200)
         .EUt(120)
         .circuit(1)
+
+    event.remove({ id:'computercraft:disk_drive' })
+    event.recipes.gtceu.assembler('computercraft:wireless_modem_advanced')
+        .itemInputs('gtceu:lv_machine_casing', '3x #forge:circuits/hv', 'gtceu:mv_electric_motor', 'gtceu:copper_small_spring', 'gtceu:magnetic_iron_rod', 'gtceu:glass_lens')
+        .inputFluids('gtceu:polyethylene 72')
+        .itemOutputs('computercraft:disk_drive')
+        .duration(200)
+        .EUt(120)
     
     // Modems
     event.remove({ id:'computercraft:wired_modem' })
