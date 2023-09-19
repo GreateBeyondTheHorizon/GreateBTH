701070d863d224aba9e0701052117cb76ea4857b
Kuba Stokowski <117862992+kStoko23@users.noreply.github.com>
2023-09-19 13:08:25 +0200

    small changes

--- kubejs/startup_scripts/Ores.js
+++ kubejs/startup_scripts/Ores.js
@@ -1,10 +1,15 @@
 // priority: 0
 
-console.info('Ores Modified')
+console.info("Ores Modified");
 
-WorldgenEvents.remove(event => {
-	event.removeOres(props => {
-	props.worldgenLayer = 'underground_ores'
-	props.blocks = [/minecraft:(.*)/, /create:(.*)/, /byg:(.*)/]
-	})
-})
+WorldgenEvents.remove((event) => {
+  /* event.removeOres((props) => {
+    props.worldgenLayer = "underground_ores";
+    props.blocks = [/minecraft:(.*)/, /create:(.*)/, /byg:(.*)/];
+  }); */
+  event.removeFeatureById("underground_ores", [
+    "create:zinc_ore",
+    "create:deepslate_zinc_ore",
+  ]);
+});
+//-8992324727273622645
