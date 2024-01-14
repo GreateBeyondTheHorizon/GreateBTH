3b32dba6ab2adf9019b7727f3594a7b5a9d73927
lonevox <quinn.epicness@gmail.com>
2024-01-14 23:42:38 +1300

    Disable some EnderIO machines
    Disabled machines are either power-related blocks or machines that have a GregTech/Create counterpart.

---
+++ kubejs/server_scripts/Mods/EnderIO.js
@@ -0,0 +1,17 @@
+ServerEvents.recipes(event => {
+	// Remove power-related blocks
+	event.remove({ output: 'enderio:stirling_generator' })
+	event.remove({ output: 'enderio:soul_engine' })
+	event.remove({ output: 'enderio:energetic_photovoltaic_module' })
+	event.remove({ output: 'enderio:pulsating_photovoltaic_module' })
+	event.remove({ output: 'enderio:vibrant_photovoltaic_module' })
+	event.remove({ output: 'enderio:wired_charger' })
+
+	// Remove machines that have a gt/create counterpart
+	event.remove({ output: 'enderio:primitive_alloy_smelter' })
+	event.remove({ output: 'enderio:alloy_smelter' })
+	event.remove({ output: 'enderio:sag_mill' })
+	event.remove({ output: 'enderio:fluid_tank' })
+	event.remove({ output: 'enderio:pressurized_fluid_tank' })
+	event.remove({ output: 'enderio:drain' })
+})
