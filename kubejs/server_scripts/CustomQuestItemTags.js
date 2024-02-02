887364de96ed0f7e6f19f14b14bdec5bde10f772
Electrolyte220 <cjm03c@gmail.com>
2024-02-02 18:28:02 -0500

    Fix broken steam extractor quest Remove ItemFilters

--- kubejs/server_scripts/CustomQuestItemTags.js
+++ kubejs/server_scripts/CustomQuestItemTags.js
@@ -2,6 +2,7 @@
 ServerEvents.tags('item', event => {
 	event.add('gbth:lp_steam_boilers', ['gtceu:lp_steam_solid_boiler', 'gtceu:lp_steam_liquid_boiler', 'gtceu:lp_steam_solar_boiler'])
 	event.add('gbth:steam_furnaces', ['gtceu:lp_steam_furnace', 'gtceu:hp_steam_furnace'])
+	event.add('gbth:steam_extractors', ['gtceu:lp_steam_extractor', 'gtceu:hp_steam_extractor'])
 	event.add('gbth:steam_macerators', ['gtceu:lp_steam_macerator', 'gtceu:hp_steam_macerator'])
 	event.add('gbth:steam_forge_hammers', ['gtceu:lp_steam_forge_hammer', 'gtceu:hp_steam_forge_hammer'])
 	event.add('gbth:steam_alloy_smelters', ['gtceu:lp_steam_alloy_smelter', 'gtceu:hp_steam_alloy_smelter'])
