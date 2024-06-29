3d12f16910a7cd1bb69a204ebfd3191cc15f01cb
lonevox <quinn.epicness@gmail.com>
2024-06-29 20:52:20 +1200

    Add straw cutting loot for tall grass

--- kubejs/server_scripts/Loot.js
+++ kubejs/server_scripts/Loot.js
@@ -1,7 +1,14 @@
 LootJS.modifiers(event => {
-    event.addBlockLootModifier('minecraft:grass').matchMainHand("#forge:tools/knives").playerAction(p => {
-			p.damageHeldItem()
-		}).randomChance(0.33).addLoot('farmersdelight:straw')
+	function addStrawCuttingLoot(blockID, chance) {
+		event.addBlockLootModifier(blockID)
+			.matchMainHand("#forge:tools/knives")
+			.playerAction(p => p.damageHeldItem())
+			.randomChance(chance)
+			.addLoot('farmersdelight:straw')
+	}
+
+	addStrawCuttingLoot('minecraft:grass', 0.33)
+	addStrawCuttingLoot('minecraft:tall_grass', 0.5)
 
 	event.addBlockLootModifier('minecraft:campfire').removeLoot('minecraft:charcoal').addLoot("2x minecraft:stick")
 	
