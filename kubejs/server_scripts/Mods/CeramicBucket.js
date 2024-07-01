8b8279c156c05ff5ebc35dab17b2f598fa221d48
lonevox <quinn.epicness@gmail.com>
2024-07-02 03:08:20 +1200

    Unfired clay bucket changes

--- kubejs/server_scripts/Mods/CeramicBucket.js
+++ kubejs/server_scripts/Mods/CeramicBucket.js
@@ -2,11 +2,9 @@ ServerEvents.recipes(event => {
 
 	event.campfireCooking('ceramicbucket:ceramic_bucket', 'ceramicbucket:unfired_clay_bucket').cookingTime(300).xp(0.3)
 
-	event.shapeless(
-		Item.of('ceramicbucket:unfired_clay_bucket'),
-			[
-				'3x gtceu:compressed_clay',
-				'#forge:tools/knives',
-			]
+	event.replaceInput(
+		'ceramicbucket:unfired_clay_bucket',
+		'minecraft:clay_ball',
+		'gtceu:compressed_clay'
 	)
 })
