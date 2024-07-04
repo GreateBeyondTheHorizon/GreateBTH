3ba204eb0968eb129a2b921c11efb1687926ee1e
Electrolyte220 <cjm03c@gmail.com>
2024-07-04 15:53:13 -0400

    Add pocketstorage & trashcans, change some recipes

--- kubejs/server_scripts/Mods/Greate/Mixer.js
+++ kubejs/server_scripts/Mods/Greate/Mixer.js
@@ -3,14 +3,4 @@ ServerEvents.recipes(event => {
 		'kubejs:unf_turtle_helmet',
 		Fluid.of('create:potion', 576, {Bottle:'REGULAR', Potion:'minecraft:water_breathing'})
 	]).heated()
-
-	event.recipes.greate.mixing('create:rose_quartz', [
-		'4x minecraft:redstone',
-		'minecraft:quartz'
-	])
-
-	event.recipes.greate.mixing(Fluid.of('gtceu:rubber', 144), [
-		Item.of('gtceu:raw_rubber_dust', 3),
-		Item.of('gtceu:sulfur_dust', 1)
-	]).heated()
 })
---
+++ kubejs/server_scripts/Mods/Greg/Mixer.js
@@ -0,0 +1,11 @@
+ServerEvents.recipes(event => {
+	event.recipes.gtceu.mixer('rose_quartz')
+		.itemInputs(['4x minecraft:redstone', 'minecraft:quartz'])
+        .itemOutputs('create:rose_quartz')
+        .EUt(3)
+
+	event.recipes.gtceu.mixer('rubber') 
+		.itemInputs(['3x gtceu:raw_rubber_dust', 'gtceu:sulfur_dust'])
+        .outputFluids(Fluid.of('gtceu:rubber', 144))
+        .EUt(3)
+})
