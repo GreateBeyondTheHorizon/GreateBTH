f9991eb0ca4a38855c9a5dce9813981c291b9821
lonevox <quinn.epicness@gmail.com>
2024-07-06 02:56:44 +1200

    Re-enable disabled blocks for crafting/decoration
    Furnaces, blast furnaces, and smokers are common blocks to decorate with, and they also are commonly found in structures. Instead of removing them and limiting builders, a tooltip on those blocks tells the player that the block has no functionality and is only used for crafting or decoration.
    
    Campfires aren't a good replacement for furnaces in many recipes (especially for the furnace minecart recipe), so I have reverted that change.
    
    I also moved furnace, blast furnace, and smoker disabling to GBTHCore to fix an issue where blocks couldn't be placed on them with shift-click.

--- kubejs/server_scripts/BlockEvents.js
+++ kubejs/server_scripts/BlockEvents.js
@@ -5,19 +5,15 @@ BlockEvents.rightClicked(event => {
 			if(mainHandItem < event.item.maxDamage) {
 				mainHandItem = mainHandItem + 1 
 				event.player.getMainHandItem().setDamageValue(mainHandItem)
-			}
-			else {
+			} else {
 				event.entity.mainHandItem = "minecraft:air"
 			}	
 			event.block.set('minecraft:campfire', { lit: 'true' })
 		}
-	} if (event.block.id === 'minecraft:crafting_table' && !event.getPlayer().stages.has('early_game_completed')) {
+	} else if (event.block.id === 'minecraft:crafting_table' && !event.getPlayer().stages.has('early_game_completed')) {
 		event.getPlayer().displayClientMessage(Component.literal("You have not unlocked the ability to use this yet!").red(), true)
 		event.cancel()
-	} if (event.block.id === 'minecraft:furnace' || event.block.id === 'minecraft:blast_furnace' || event.block.id === 'minecraft:smoker') {
-		event.getPlayer().displayClientMessage(Component.literal("This block has no functionality!").red(), true)
-		event.cancel()
-	} if (event.block.tags.contains(new ResourceLocation('beds')) && !event.getPlayer().stages.has('crafted_bed')) {
+	} else if (event.block.tags.contains(new ResourceLocation('beds')) && !event.getPlayer().stages.has('crafted_bed')) {
 		event.getPlayer().displayClientMessage(Component.literal("You have not unlocked the ability to use this yet!").red(), true)
 		event.cancel()
 	}
