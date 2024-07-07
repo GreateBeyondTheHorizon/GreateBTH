7f66c3f814cdae2b024fd48807db00d3cc971c53
Electrolyte220 <cjm03c@gmail.com>
2024-07-07 13:54:56 -0400

    Allow coating wires using the spout

--- kubejs/server_scripts/Mods/Greg/Create.js
+++ kubejs/server_scripts/Mods/Greg/Create.js
@@ -1,3 +1,15 @@
 ServerEvents.recipes(event => {
     event.remove({id: 'create/splashing/magma_block'})
+
+    //TODO: move to greate
+    event.forEachRecipe({type: 'gtceu:assembler', id: /gtceu:assembler(.*)cover(.*)wire(.*)/}, c => {
+        var items = c.json.get('inputs').getAsJsonObject().get('item').getAsJsonArray()
+        var fluids = c.json.get('inputs').getAsJsonObject().get('fluid').getAsJsonArray().get(0).getAsJsonObject().get('content').getAsJsonObject()
+        var output = c.json.get('outputs').getAsJsonObject().get('item').getAsJsonArray().get(0).getAsJsonObject().get('content').getAsJsonObject().get('ingredient').getAsJsonObject().get('item')
+        if(items.size() == 1) {
+            var item = items.get(0).getAsJsonObject().get('content').getAsJsonObject().get('ingredient').getAsJsonObject().get('item')
+            var fluid = fluids.get('value').getAsJsonArray().get(0).getAsJsonObject().get('tag').getAsString()
+            event.recipes.create.filling(output, [Fluid.of(fluid.replace('forge', 'gtceu'), fluids.get('amount')), item])
+        }
+    })
 })
