aa167c85144ab5e5ccee5e04399b2014bffabad8
lonevox <quinn.epicness@gmail.com>
2024-07-11 13:05:33 +1200

    Unlock crafting table usage from quest instead of crafting it
    This makes the crafting table unlocked for all players in a guild when one player crafts one because the players share quests. Before, each player needed to craft a crafting table to be able to use it.

--- kubejs/server_scripts/ItemEvents.js
+++ kubejs/server_scripts/ItemEvents.js
@@ -1,10 +1,5 @@
 ItemEvents.crafted(event => {
-	if(event.getItem().id === 'minecraft:crafting_table') {
-		if(!event.getPlayer().stages.has('early_game_completed')) {
-			event.getPlayer().stages.add('early_game_completed')
-			event.getPlayer().displayClientMessage(Component.literal('You have unlocked the ability to use a crafting table.'), true)
-		}
-	} if(event.getItem().id.includes('_bed')) {
+	if(event.getItem().id.includes('_bed')) {
 		if(!event.getPlayer().stages.has('crafted_bed')) {
 			event.getPlayer().stages.add('crafted_bed')
 			event.getPlayer().displayClientMessage(Component.literal('You have unlocked the ability to use a bed.'), true)
