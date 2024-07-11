e5875a3676f6e679d900f56a25f1e8ad39d09f2c
lonevox <quinn.epicness@gmail.com>
2024-07-11 13:34:59 +1200

    Reduce players required to sleep to 50%

--- kubejs/server_scripts/Gamerules.js
+++ kubejs/server_scripts/Gamerules.js
@@ -1,4 +1,5 @@
 ServerEvents.loaded(e => {
     e.server.runCommand("gamerule doInsomnia false")
     e.server.runCommand("gamerule doPatrolSpawning false")
+    e.server.runCommand("gamerule playersSleepingPercentage 50")
 })
