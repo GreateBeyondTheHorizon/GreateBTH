0786b2ae0a573f0747b4d7c526e40179a707e54f
Electrolyte220 <cjm03c@gmail.com>
2024-07-18 23:52:17 -0400

    Allow more modded wood-type blocks to be macerated in to wood dust Downgrade EMI to show Greate tabs

--- kubejs/server_scripts/Mods/Greate/Milling.js
+++ kubejs/server_scripts/Mods/Greate/Milling.js
@@ -1,5 +1,16 @@
 ServerEvents.recipes(event => {
-    event.recipes.greate.milling('gtceu:obsidian_dust', [
-        'minecraft:obsidian'
-    ]).recipeTier(1)
+
+    event.remove({type: 'gtceu:macerator', id: '/gtceu:(.*)macerate(.*)planks/'})
+    event.remove({type: 'gtceu:macerator', id: '/gtceu:(.*)macerate(.*)fence/'})
+    event.remove({type: 'gtceu:macerator', id: '/gtceu:(.*)macerate(.*)boat/'})
+    event.remove({type: 'gtceu:macerator', id: '/gtceu:(.*)macerate(.*)fence_gate/'})
+    event.remove({type: 'gtceu:macerator', id: '/gtceu:(.*)macerate(.*)door/'})
+
+    event.recipes.greate.milling('gtceu:obsidian_dust', ['minecraft:obsidian']).recipeTier(1)
+
+    event.recipes.greate.milling('gtceu:wood_dust', ['#minecraft:planks'])
+    event.recipes.greate.milling('gtceu:wood_dust', ['#minecraft:fences'])
+    event.recipes.greate.milling('5x gtceu:wood_dust', ['#minecraft:boats'])
+    event.recipes.greate.milling('3x gtceu:wood_dust', ['#minecraft:fence_gates'])
+    event.recipes.greate.milling(['2x gtceu:wood_dust', 'gtceu:tiny_iron_dust'], ['#minecraft:wooden_doors'])
 })
