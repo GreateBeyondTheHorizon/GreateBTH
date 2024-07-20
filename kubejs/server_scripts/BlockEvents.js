839640bdcdcf37e875ea605f1f1d14a24168d7dc
lonevox <quinn.epicness@gmail.com>
2024-07-21 01:37:33 +1200

    Separate BlockEvents
    This apparently is better for CPU usage. It is at least better organised.

--- kubejs/server_scripts/BlockEvents.js
+++ kubejs/server_scripts/BlockEvents.js
@@ -1,5 +1,5 @@
-BlockEvents.rightClicked(event => {
-	if(event.block.id === 'minecraft:campfire' && event.item.id === 'kubejs:fire_starter') {
+BlockEvents.rightClicked('minecraft:campfire', event => {
+	if(event.item.id === 'kubejs:fire_starter') {
 		if (event.block.properties.get('lit') !== true) {
 			var mainHandItem = event.player.getMainHandItem().getDamageValue()
 			if(mainHandItem < event.item.maxDamage) {
@@ -10,10 +10,18 @@ BlockEvents.rightClicked(event => {
 			}	
 			event.block.set('minecraft:campfire', { lit: 'true' })
 		}
-	} else if (event.block.id === 'minecraft:crafting_table' && !event.getPlayer().stages.has('early_game_completed')) {
+	}
+})
+
+BlockEvents.rightClicked('minecraft:crafting_table', event => {
+	if (!event.getPlayer().stages.has('early_game_completed')) {
 		event.getPlayer().displayClientMessage(Component.literal("Complete the Introduction quest chapter to be able to use this!").red(), true)
 		event.cancel()
-	} else if (event.block.tags.contains(new ResourceLocation('beds')) && !event.getPlayer().stages.has('crafted_bed')) {
+	}
+})
+
+BlockEvents.rightClicked(event => {
+	if (event.block.tags.contains(new ResourceLocation('beds')) && !event.getPlayer().stages.has('crafted_bed')) {
 		event.getPlayer().displayClientMessage(Component.literal('Complete the "On the Floor No-More" quest to be able to use this!').red(), true)
 		event.cancel()
 	}
