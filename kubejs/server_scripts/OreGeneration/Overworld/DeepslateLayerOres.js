4aebff851c22f0336fef30d38d3d73bb65c29c3e
Electrolyte220 <cjm03c@gmail.com>
2024-11-30 17:36:13 -0500

    Adjust ore veins & add coal stone vein

--- kubejs/server_scripts/OreGeneration/Overworld/DeepslateLayerOres.js
+++ kubejs/server_scripts/OreGeneration/Overworld/DeepslateLayerOres.js
@@ -1,4 +1,4 @@
-addVein('copper_vein', GTWorldGenLayers.DEEPSLATE, 25, 80, -40, 10, GTMaterials.Copper, 'surface', pattern => {
+addVein('copper_vein', GTWorldGenLayers.DEEPSLATE, 25, 80, -40, -10, GTMaterials.Copper, 'surface', pattern => {
     pattern
         .layer(l => l.weight(5).mat(GTMaterials.Chalcopyrite).size(2, 2))
         .layer(l => l.weight(2).mat(GTMaterials.Iron).size(1, 1))
@@ -6,14 +6,14 @@ addVein('copper_vein', GTWorldGenLayers.DEEPSLATE, 25, 80, -40, 10, GTMaterials.
         .layer(l => l.weight(2).mat(GTMaterials.Copper).size(1, 1))
 })
 
-addVein('diamond_vein', GTWorldGenLayers.DEEPSLATE, 25, 40, -65, 30, GTMaterials.Diamond, 'above', pattern => {
+addVein('diamond_vein', GTWorldGenLayers.DEEPSLATE, 25, 40, -65, -20, GTMaterials.Diamond, 'above', pattern => {
     pattern
         .layer(l => l.weight(3).mat(GTMaterials.Graphite).size(2, 2))
         .layer(l => l.weight(2).mat(GTMaterials.Diamond).size(1, 1))
         .layer(l => l.weight(1).mat(GTMaterials.Coal).size(1, 1))
 })
 
-addVein('lapis_vein', GTWorldGenLayers.DEEPSLATE, 25, 40, -60, 10, GTMaterials.Lapis, 'above', pattern => {
+addVein('lapis_vein', GTWorldGenLayers.DEEPSLATE, 25, 40, -60, -10, GTMaterials.Lapis, 'above', pattern => {
     pattern
         .layer(l => l.weight(3).mat(GTMaterials.Lazurite).size(2, 2))
         .layer(l => l.weight(2).mat(GTMaterials.Sodalite).size(1, 1))
@@ -21,7 +21,7 @@ addVein('lapis_vein', GTWorldGenLayers.DEEPSLATE, 25, 40, -60, 10, GTMaterials.L
         .layer(l => l.weight(1).mat(GTMaterials.Calcite).size(1, 1))
 })
 
-addVein('manganese_vein', GTWorldGenLayers.DEEPSLATE, 30, 20, -30, 0, GTMaterials.Grossular, 'surface', pattern => {
+addVein('manganese_vein', GTWorldGenLayers.DEEPSLATE, 30, 20, -30, -10, GTMaterials.Grossular, 'surface', pattern => {
     pattern
         .layer(l => l.weight(3).mat(GTMaterials.Grossular).size(2, 2))
         .layer(l => l.weight(2).mat(GTMaterials.Spessartine).size(1, 1))
@@ -37,7 +37,7 @@ addVein('mica_vein', GTWorldGenLayers.DEEPSLATE, 25, 20, -40, -10, GTMaterials.M
         .layer(l => l.weight(1).mat(GTMaterials.Pollucite).size(1, 1))
 })
 
-addVein('olivine_vein', GTWorldGenLayers.DEEPSLATE, 25, 20, -20, 10, GTMaterials.Olivine, 'surface', pattern => {
+addVein('olivine_vein', GTWorldGenLayers.DEEPSLATE, 25, 20, -30, -10, GTMaterials.Olivine, 'surface', pattern => {
     pattern
         .layer(l => l.weight(3).mat(GTMaterials.Bentonite).size(2, 2))
         .layer(l => l.weight(2).mat(GTMaterials.Magnetite).size(1, 1))
@@ -52,7 +52,7 @@ addVein('redstone_vein', GTWorldGenLayers.DEEPSLATE, 25, 60, -65, -10, GTMateria
         .layer(l => l.weight(1).mat(GTMaterials.Cinnabar).size(1, 1))
 })
 
-addVein('sapphire_vein', GTWorldGenLayers.DEEPSLATE, 20, 60, -40, 0, GTMaterials.Sapphire, 'above', pattern => {
+addVein('sapphire_vein', GTWorldGenLayers.DEEPSLATE, 20, 60, -40, -10, GTMaterials.Sapphire, 'above', pattern => {
     pattern
         .layer(l => l.weight(3).mat(GTMaterials.Almandine).size(2, 2))
         .layer(l => l.weight(2).mat(GTMaterials.Pyrope).size(1, 1))
