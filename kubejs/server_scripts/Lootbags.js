51df59e4c81f6fceebdf110654bc68233cfef337
Electrolyte220 <cjm03c@gmail.com>
2025-01-24 23:51:50 -0500

    Remove food related quests & lootbags

--- kubejs/server_scripts/Lootbags.js
+++ kubejs/server_scripts/Lootbags.js
@@ -10,28 +10,10 @@ ServerEvents.recipes(event => {
 
 	event.custom({
 		type: 'lootbags:loot',
-		name: 'Lootbag (Common Food)',
-		rarity: 'COMMON',
-		output: {
-			table: 'gbth:chests/common_food'
-		}
-	})
-
-	event.custom({
-		type: 'lootbags:loot',
 		name: 'Lootbag (ULV Age)',
 		rarity: 'COMMON',
 		output: {
 			table: 'gbth:chests/ulv_age'
-		}
-	})
-
-	event.custom({
-		type: 'lootbags:loot',
-		name: 'Lootbag (Tasty Foods)',
-		rarity: 'UNCOMMON',
-		output: {
-			table: 'gbth:chests/tasty_foods'
 		}
 	})
 })
