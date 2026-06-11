0500072d686c0764bf5807fde5275b9a22c20a7b
Electrolyte <21316184+Electrolyte220@users.noreply.github.com>
2026-06-11 17:55:40 -0400

    wrong file name

--- packwiz-download-latest.sh
+++ packwiz-download-latest.sh
@@ -1,1 +1,1 @@
-wget -O "packwiz-installer.jar" "https://github.com/packwiz/packwiz-installer/releases/download/v0.5.14/packwiz.jar"
+wget -O "packwiz.jar" "https://github.com/packwiz/packwiz-installer/releases/download/v0.5.14/packwiz-installer.jar"
