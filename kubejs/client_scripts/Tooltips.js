5eb975ac611e3ac9a1f1d19aa553f233d8710e1e
Electrolyte <21316184+Electrolyte220@users.noreply.github.com>
2026-07-23 12:09:29 -0400

    Fix tooltips script

--- kubejs/client_scripts/Tooltips.js
+++ kubejs/client_scripts/Tooltips.js
@@ -1,26 +1,4 @@
 ItemEvents.tooltip((event) => {
-  event.addAdvanced("kubejs:removed_tag_placeholder", (item, advanced, text) => {
-    let removedTag = item.nbt?.Removed;
-    if (removedTag != undefined) {
-      text.add(Text.darkGray("Removed: ")
-        .append(Text.lightPurple(removedTag)));
-    }
-  });
-  event.addAdvanced("kubejs:removed_item_placeholder", (item, advanced, text) => {
-    let removedItem = item.nbt?.Removed;
-    if (removedItem != undefined) {
-      text.add(Text.darkGray("Removed: ")
-        .append(Text.lightPurple(removedItem)));
-    }
-  });
-
-  for (const item of global.ItemsToRemove) {
-    global.Util.forEachItemExpanded(item, (itemId) => {
-      event.add(itemId, Text.red("DISABLED.")
-        .append(Text.gray(" If you have found this, report it as a bug.")));
-    });
-  }
-
   event.add(["minecraft:furnace", "minecraft:blast_furnace", "minecraft:smoker"], [["", Text.yellow("No functionality."), Text.gray(" Used for crafting or decoration only.")]]);
   event.add(
     [
